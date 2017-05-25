72953c50167e93065728661a06d81eae9f1d3074
joseoyoung <josy3465@naver.com>
2017-05-25 20:49:40 +0900

    #1 Change Layout and Event Programming

--- app/src/main/java/kr/hs/emirim/joseoyoung/dialogtest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/joseoyoung/dialogtest/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.joseoyoung.dialogtest;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,12 +8,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    String[] itemArr={"오이카와 토오루","우시지마 와카토시","쿠로오 테츠로","스가와라 코우시"};
+    Button butDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog=(Button)findViewById(R.id.but_dialog);
+        butDialog=(Button)findViewById(R.id.but_dialog);
+        butDialog=(Button)findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);
     }
 
@@ -20,9 +23,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);
         dialog.setTitle("첫번째 다이얼로그");
-        //dialog.setIcon();
+        //dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
         dialog.setIcon(R.mipmap.ic_launcher);//기본 아이콘
-        dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
+        dialog.setItems(itemArr, null);
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });
         dialog.setPositiveButton("OK", null);
         dialog.show();
     }
