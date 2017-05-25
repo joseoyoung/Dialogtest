fd10ae624548856437f081a952cf917575cf4abe
joseoyoung <josy3465@naver.com>
2017-05-25 20:29:29 +0900

    #2 Create Dialog

--- app/src/main/java/kr/hs/emirim/joseoyoung/dialogtest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/joseoyoung/dialogtest/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.joseoyoung.dialogtest;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("첫번째 다이얼로그");
+        //dialog.setIcon();
+        dialog.setIcon(R.mipmap.ic_launcher);//기본 아이콘
+        dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
+        dialog.setPositiveButton("OK", null);
+        dialog.show();
     }
 }
